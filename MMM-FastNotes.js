1f9418920497ccb9aa0cfe811e73d5b1bd672423
krukle <christofferfareseid@gmail.com>
2022-07-08 12:50:15 +0200

    move controller fnctionality to mycroft skill

--- MMM-FastNotes.js
+++ MMM-FastNotes.js
@@ -1,44 +1,55 @@
 Module.register("MMM-FastNotes", {
-    defaults: {
-        // updateInterval: 5000, // request DB every N miliseconds (5 sec by default)
-        pythonPath: 'python3',  // shell command or path to Python 3.6 (or higher) binary
-        // css: 0,  // 0 - current default MM CSS styles; 1 - custom css stile
-        // TextStyle could be "xsmall bright" or "medium bright" or another one defined in /home/pi/MagicMirror/css/main.css, or in your own custom CSS
-        contentTextStyle: "normal dimmed", // style for Note Content
-        // animationSpeed: 2.5 * 1000,  // speed of text fading and changing in 2.5 sec
-    },
+    // defaults: {
+    //     // contentTextStyle: "normal dimmed", // style for Note Content
+    // },
 
     start: function () {
-        this.sendSocketNotification("START_FAST_NOTES");
+        this.sendSocketNotification("HEJ");
         this.notepad = [];
-        this.updateDom()
+        console.log(0)
     },
 
     socketNotificationReceived: function (notification, payload) {
-        if (notification === "DATABASE") {
-            this.notepad = payload;
-            this.updateDom()
-            console.log("Notification received!")
-        } 
+        if (notification === "NEW-POST") {
+            this.notepad.push(payload.post)
+        } else if (notification === "ALL-POSTS") {
+            this.notepad = payload.posts;
+        } else if (notification === "DELETE-POSTS") {
+            this.notepad = [];
+        }
+        console.log(payload)
+        console.log(1)
+        this.updateDom();
+    },
+
+    notificationReceived: function(notification, payload, sender) {
+        if (notification === "MYCROFT_CONNECTED") {
+            console.log(10)
+            this.sendNotification("MYCROFT_COMMAND", {
+                eventName: "fastnotes-skill:get_all_posts",
+                data: {}
+            });
+        }
     },
 
     getDom: function () {
+        console.log(2)
         var wrapper = document.createElement("div");
         const noteList = document.createElement('ul');
         wrapper.innerHTML = "";
         if (this.notepad.length > 0) {
-            console.log("Notepad longer than 0")
+            console.log(3)
             this.notepad.forEach((item) => {
                 const note = document.createElement('li');
-                note.innerText = item.Text;
-                note.className = this.config.contentTextStyle;
+                note.innerText = item;
                 noteList.appendChild(note);
             });
             wrapper.appendChild(noteList);
         } else {
-            console.log("Notepad is 0")
+            console.log(4)
             wrapper.innerHTML = "No Notes...";
         }
+        console.log(5)
         return wrapper;
     }
 });
